215dfccaec620378e5072627b0bcba714b7b0028
bagaspardanailham18 <ibagaspardana@gmail.com>
2023-01-13 07:25:00 +0700

    update Ecommerce Repository

--- app/src/main/java/com/bagaspardanailham/myecommerceapp/data/EcommerceRepository.kt
+++ app/src/main/java/com/bagaspardanailham/myecommerceapp/data/EcommerceRepository.kt
@@ -28,30 +28,6 @@ class EcommerceRepository @Inject constructor(private val apiService: ApiService
         const val API_KEY = "TuIBt77u7tZHi8n7WqUC"
     }
 
-//    suspend fun registerUser(name: String, email: String, password: String, phone: String, image: String, gender: Int): LiveData<Result<RegisterResponse>> = liveData(Dispatchers.IO) {
-//        emit(Result.Loading)
-//        val response = apiService.registerUser(API_KEY, name, email, password, phone, gender, image)
-//        try {
-//            Log.d("result", response.success?.message.toString())
-//            emit(Result.Success(response))
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            emit(Result.Error(response.error?.message.toString()))
-//        }
-//    }
-//
-//    suspend fun loginUser(email: String, password: String) = liveData(Dispatchers.IO) {
-//        emit(Result.Loading)
-//        val response = apiService.loginUser(API_KEY, email, password)
-//        try {
-//            Log.d("result", response.success?.message.toString())
-//            emit(Result.Success(response))
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            emit(Result.Error(response.error?.message.toString()))
-//        }
-//    }
-
     suspend fun registerUser(name: String, email: String, password: String, phone: String, image: String, gender: Int): LiveData<Result<RegisterResponse>> = liveData {
         emit(Result.Loading)
         try {
@@ -105,58 +81,4 @@ class EcommerceRepository @Inject constructor(private val apiService: ApiService
             }
         }
     }
-
-//        suspend fun loginUser(email: String, password: String): LiveData<Result<LoginResponse>> = liveData {
-//        emit(Result.Loading)
-//            val response = apiService.loginUser(API_KEY, email, password)
-//            response.enqueue(object: Callback<LoginResponse> {
-//                override fun onResponse(
-//                    call: Call<LoginResponse>,
-//                    response: Response<LoginResponse>
-//                ) {
-//                    val responseBody = response.body()
-//                    if (response.isSuccessful) {
-//                        if (responseBody != null) {
-//                            Result.Success(responseBody)
-//                        }
-//                    } else {
-//                        if (responseBody?.success?.status == 400) {
-//                            Result.Success(responseBody.success.message)
-//                        }
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-//                    Result.Error(t.message.toString())
-//                }
-//            })
-//    }
-
-
-//    fun registerUser(name: String, email: String, password: String, phone: String, image: String, gender: String): LiveData<Result<RegisterResponse>> = liveData {
-//        Result.Loading
-//        try {
-//            val response = apiService.registerUser(API_KEY, name, email, password, phone, gender, image)
-//            response.enqueue(object: Callback<RegisterResponse> {
-//                override fun onResponse(
-//                    call: Call<RegisterResponse>,
-//                    response: Response<RegisterResponse>
-//                ) {
-//                    if (response.isSuccessful) {
-//                        val responseBody = response.body()
-//                        if (responseBody != null) {
-//                            Result.Success(responseBody)
-//                        }
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
-//                    Result.Error(t.message.toString())
-//                }
-//            })
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            Result.Error(e.message.toString())
-//        }
-//    }
 }
