cafcfbbc1a0fbbe7bfdd5f3ed48f3d3b11bc3384
bagaspardanailham18 <ibagaspardana@gmail.com>
2023-01-13 10:51:31 +0700

    update profile fragment UI

--- app/src/main/java/com/bagaspardanailham/myecommerceapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bagaspardanailham/myecommerceapp/ui/profile/ProfileFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bagaspardanailham.myecommerceapp.R
 import com.bagaspardanailham.myecommerceapp.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,6 +30,14 @@ class ProfileFragment : Fragment() {
         val root: View = binding.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val items = listOf("English", "Indonesia")
+        val adapter = ArrayAdapter(requireContext(), R.layout.item_row_language, items)
+        (binding.edtLang as? AutoCompleteTextView)?.setAdapter(adapter)
     }
 
     override fun onDestroyView() {
