a059b5964dfe1e13f5a473f6a614208f1efb451d
bagaspardanailham18 <ibagaspardana@gmail.com>
2023-01-15 20:46:13 +0700

    create pick image feature from camera and gallery

--- app/src/main/java/com/bagaspardanailham/myecommerceapp/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/bagaspardanailham/myecommerceapp/ui/auth/register/RegisterFragment.kt
@@ -1,8 +1,17 @@
 package com.bagaspardanailham.myecommerceapp.ui.auth.register
 
 import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.util.Patterns
 import androidx.fragment.app.Fragment
@@ -11,6 +20,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -21,11 +31,18 @@ import com.bagaspardanailham.myecommerceapp.databinding.FragmentRegisterBinding
 import kotlinx.coroutines.launch
 import com.bagaspardanailham.myecommerceapp.data.Result
 import com.bagaspardanailham.myecommerceapp.data.remote.response.ErrorResponse
+import com.bagaspardanailham.myecommerceapp.ui.CameraActivity
 import com.bagaspardanailham.myecommerceapp.ui.auth.AuthViewModel
+import com.bagaspardanailham.myecommerceapp.utils.createCustomTempFile
+import com.bagaspardanailham.myecommerceapp.utils.rotateBitmap
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import org.json.JSONObject
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 
 @AndroidEntryPoint
 class RegisterFragment : Fragment() {
@@ -34,6 +51,28 @@ class RegisterFragment : Fragment() {
     private val binding get() = _binding
 
     private val registerViewModel by viewModels<AuthViewModel>()
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionGranted()) {
+                Toast.makeText(
+                    requireActivity(),
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                requireActivity().finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,6 +85,12 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
 
         binding?.imgPickerBtn?.setOnClickListener {
             showImgPickerDialog()
@@ -79,33 +124,85 @@ class RegisterFragment : Fragment() {
 
     }
 
-    private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permission ->
-            when {
-                permission[Manifest.permission.CAMERA] ?: false -> {
-                    getImgFromCamera()
-                }
-            }
-        }
+//    private val launcherIntentCamera = registerForActivityResult(
+//        ActivityResultContracts.StartActivityForResult()
+//    ) {
+//        if (it.resultCode == RESULT_OK) {
+//            val imageBitmap = it.data?.extras?.get("data") as Bitmap
+//            binding?.tvUserImg?.setImageBitmap(imageBitmap)
+//        }
+//    }
 
-    private fun checkSelfPermission(permission: String): Boolean {
-        return ContextCompat.checkSelfPermission(
-            requireActivity(),
-            permission
-        ) == PackageManager.PERMISSION_GRANTED
-    }
+//    private val requestPermissionLauncher =
+//        registerForActivityResult(
+//            ActivityResultContracts.RequestMultiplePermissions()
+//        ) { permission ->
+//            when {
+//                permission[Manifest.permission.CAMERA] ?: false -> {
+//                    getImgFromCamera()
+//                }
+//            }
+//        }
+//
+//    private fun checkSelfPermission(permission: String): Boolean {
+//        return ContextCompat.checkSelfPermission(
+//            requireActivity(),
+//            permission
+//        ) == PackageManager.PERMISSION_GRANTED
+//    }
 
     private fun getImgFromCamera() {
-        if (checkSelfPermission(CAMARA_PERMISSION)) {
+        val intent = Intent(requireActivity(), CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
 
-        } else {
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
 
+            binding?.tvUserImgPrev?.setImageBitmap(result)
         }
     }
 
     private fun getImgFromGallery() {
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    fun uriToFile(selectedImg: Uri, context: Context): File {
+        val contentResolver: ContentResolver = context.contentResolver
+        val myFile = createCustomTempFile(context)
+
+        val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+        val outputStream: OutputStream = FileOutputStream(myFile)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+        outputStream.close()
+        inputStream.close()
+
+        return myFile
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, requireContext())
+            binding?.tvUserImgPrev?.setImageURI(selectedImg)
+        }
 
     }
 
@@ -185,6 +282,8 @@ class RegisterFragment : Fragment() {
     }
 
     companion object {
-        const val CAMARA_PERMISSION = Manifest.permission.CAMERA
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
