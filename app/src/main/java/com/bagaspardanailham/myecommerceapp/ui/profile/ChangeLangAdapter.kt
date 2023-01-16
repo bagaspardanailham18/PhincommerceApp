833f8326283ecd744abc9f174fbe243c2355a813
bagaspardanailham18 <ibagaspardana@gmail.com>
2023-01-16 10:18:14 +0700

    fix spinner change language

---
+++ app/src/main/java/com/bagaspardanailham/myecommerceapp/ui/profile/ChangeLangAdapter.kt
@@ -0,0 +1,32 @@
+package com.bagaspardanailham.myecommerceapp.ui.profile
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bagaspardanailham.myecommerceapp.R
+
+class ChangeLangAdapter(val context: Context, val names: Array<String>, val imgs: IntArray): BaseAdapter() {
+    private var inflater = LayoutInflater.from(context)
+    override fun getCount(): Int = imgs.size
+
+    override fun getItem(p0: Int): Any? = null
+
+    override fun getItemId(p0: Int): Long {
+        return 0
+    }
+
+    @SuppressLint("ViewHolder")
+    override fun getView(i: Int, p1: View?, p2: ViewGroup?): View {
+        val view = inflater.inflate(R.layout.item_row_language, null)
+        val langText = view.findViewById<TextView>(R.id.lang_text)
+        val langImg = view.findViewById<ImageView>(R.id.lang_img)
+        langText.text = names[i]
+        langImg.setImageResource(imgs[i])
+        return view
+    }
+}
--- app/src/main/java/com/bagaspardanailham/myecommerceapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bagaspardanailham/myecommerceapp/ui/profile/ProfileFragment.kt
@@ -1,6 +1,13 @@
 package com.bagaspardanailham.myecommerceapp.ui.profile
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,17 +16,29 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.bagaspardanailham.myecommerceapp.R
 import com.bagaspardanailham.myecommerceapp.databinding.FragmentProfileBinding
+import com.bagaspardanailham.myecommerceapp.ui.CameraActivity
 import com.bagaspardanailham.myecommerceapp.ui.auth.AuthActivity
 import com.bagaspardanailham.myecommerceapp.ui.auth.AuthViewModel
+import com.bagaspardanailham.myecommerceapp.ui.auth.register.RegisterFragment
+import com.bagaspardanailham.myecommerceapp.utils.createCustomTempFile
+import com.bagaspardanailham.myecommerceapp.utils.rotateBitmap
 import com.bumptech.glide.Glide
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
@@ -31,6 +50,30 @@ class ProfileFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<AuthViewModel>()
+
+    private var getFile: File? = null
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionGranted()) {
+                Toast.makeText(
+                    requireActivity(),
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                requireActivity().finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,9 +89,19 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val items = listOf("EN", "IDN")
-        val adapter = ArrayAdapter(requireContext(), R.layout.item_row_language, items)
-        (binding.edtLang as? AutoCompleteTextView)?.setAdapter(adapter)
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        val langNames = arrayOf("EN", "IDN")
+        val langImgs = intArrayOf(R.drawable.us_flag, R.drawable.idn_flag)
+        val adapter = ChangeLangAdapter(requireContext(), langNames, langImgs)
+
+        binding.langSpinner.adapter = adapter
 
         setProfile()
         setAction()
@@ -69,6 +122,22 @@ class ProfileFragment : Fragment() {
     }
 
     private fun setAction() {
+        binding.imgPickerBtn.setOnClickListener {
+            val items = arrayOf("Camera", "Gallery")
+            MaterialAlertDialogBuilder(requireActivity())
+                .setTitle("Select Image")
+                .setItems(items) { dialog, which ->
+                    when {
+                        items[which] == "Camera" -> {
+                            getImgFromCamera()
+                        }
+                        items[which] == "Gallery" -> {
+                            getImgFromGallery()
+                        }
+                    }
+                }
+                .show()
+        }
         binding.btnToChangePassword.setOnClickListener {
             requireActivity().startActivity(Intent(requireActivity(), ChangePasswordActivity::class.java))
         }
@@ -82,8 +151,71 @@ class ProfileFragment : Fragment() {
         }
     }
 
+    private fun getImgFromCamera() {
+        val intent = Intent(requireActivity(), CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            getFile = myFile
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+
+            binding.tvUserImg.setImageBitmap(result)
+        }
+    }
+
+    private fun getImgFromGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    fun uriToFile(selectedImg: Uri, context: Context): File {
+        val contentResolver: ContentResolver = context.contentResolver
+        val myFile = createCustomTempFile(context)
+
+        val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+        val outputStream: OutputStream = FileOutputStream(myFile)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+        outputStream.close()
+        inputStream.close()
+
+        return myFile
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+
+        if (result.resultCode == Activity.RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, requireContext())
+            getFile = myFile
+            binding.tvUserImg.setImageURI(selectedImg)
+        }
+
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
